16b8f5471bbd8c3f044de37e29456dd7373e7d64
Vallipuram Selvanesan <vselvane@my.centennialcollege.ca>
2018-06-06 12:47:12 -0400

    after update the contents

---
+++ Scripts/contact.js
@@ -0,0 +1,40 @@
+(function(content){
+
+    function ContactContent() {
+        console.log("%c Contact Content Accessed...", "font-weight:bold; font-size: 20px;");
+        
+        document.getElementsByClassName("card-title")[0].textContent = "Contact You!";
+
+        // create a new HTML Element
+        let cancelButton = document.createElement("button");
+        // configure the HTML Element
+        cancelButton.setAttribute("class", "btn btn-warning");
+        cancelButton.classList.add("btn-lg");
+        cancelButton.textContent = "Cancel";
+        cancelButton.addEventListener("click", function(event){
+            event.preventDefault();
+            window.open("index.html","_parent");
+        });
+
+        // add the HTML Element to the page somewhere 
+        // in this case I'm attaching a button to the first forml element
+        document.forms[0].appendChild(cancelButton);
+
+        document.title = "Contact";
+        // use the history object to wipe out the url 
+        history.pushState("", document.title, "/contact");
+
+
+        // use the location object to output my pathname
+        console.log(location.pathname);
+
+        // show your hostname - domain name
+        console.log(location.hostname);
+
+    }
+
+    content.ContactContent = ContactContent;
+   
+})(content || (content = {}));
+
+
